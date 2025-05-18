d244b6ce797dddb5fbe23237b4573246952b272b
Chiichen <chiichen@qq.com>
2025-05-18 22:33:22 +0800

    feat(mmkv-sys): build mmkv static lib with cmake

--- crates/mmkv-sys/build.rs
+++ crates/mmkv-sys/build.rs
@@ -1,24 +1,45 @@
 use std::{env, path::PathBuf};
 
 fn main() {
-    let mut config = cmake::Config::new("MMKV/Core");
-    let dst = config.build();
-    println!("cargo:rustc-link-lib=staticlib=core");
-    println!("cargo:rustc-link-search={}", dst.display());
-    println!("cargo:rustc-link-search={}", dst.join("lib").display());
-    let bindings = bindgen::Builder::default()
-        .header("MMKV/Core/MMKV.h")
-        .use_core()
-        .clang_args([
-            format!("-I{}/{}", dst.display(), "include"),
-            #[cfg(target_os = "windows")]
-            r#"-DWIN32"#.to_string(), // As Above
-        ])
-        .generate()
-        .expect("unable to generate rfb bindings");
+    // Path to MMKV Core source
+    let mmkv_core = PathBuf::from("MMKV/Core");
 
+    // Configure and build MMKV Core using CMake
+    let dst = cmake::Config::new(&mmkv_core)
+        .define("CMAKE_BUILD_TYPE", "Release")
+        .build_target("core")
+        .build();
+
+    // Tell cargo where to find the built library
+    println!("cargo:rustc-link-search=native={}", dst.display());
+    let mmkv_core_include = mmkv_core.join("include/MMKV");
+    // Generate bindings
+    let bindings = bindgen::Builder::default()
+        // The input header we would like to generate bindings for
+        .header(mmkv_core_include.join("MMKV.h").to_str().unwrap())
+        // .header(mmkv_core_include.join("MMKVPredef.h").to_str().unwrap())
+        // .header(mmkv_core_include.join("MMBuffer.h").to_str().unwrap())
+        // .header(mmkv_core_include.join("MiniPBCoder.h").to_str().unwrap())
+        .use_core()
+        .clang_args(["-x", "c++"])
+        .clang_arg("-std=c++17")
+        .layout_tests(false) //Layout tests are failed. Skip it for now
+        // .parse_callbacks(Box::new(bindgen::CargoCallbacks::new()))
+        // https://github.com/rust-lang/rust-bindgen/issues/2157
+        // .clang_arg("-stdlib=libc++")
+        // .no_copy("std\\_vector\\_\\_Temporary_value\\_\\_Storage")
+        // .manually_drop_union("std\\_vector\\_\\_Temporary_value\\_\\_Storage")
+        // .default_non_copy_union_style(bindgen::NonCopyUnionStyle::ManuallyDrop)
+        .allowlist_type("MMKV")
+        .opaque_type("MMKV")
+        .opaque_type("^(std.*)$")
+        .formatter(bindgen::Formatter::Prettyplease)
+        .generate()
+        .expect("Unable to generate bindings");
+
+    // Write the bindings to the $OUT_DIR/bindings.rs file
     let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
     bindings
         .write_to_file(out_path.join("bindings.rs"))
-        .expect("couldn't write bindings!");
+        .expect("Couldn't write bindings!");
 }
